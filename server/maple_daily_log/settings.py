546f7e5eb1a8c14fce3a67f043ea7db4f2ed6ef1
김유찬 <a01030163474@gmail.com>
2026-07-09 15:52:32 +0900

    fix: health 인증 초기화 오류 수정

--- server/maple_daily_log/settings.py
+++ server/maple_daily_log/settings.py
@@ -34,6 +34,8 @@ REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.IsAuthenticated",
     ],
+    "UNAUTHENTICATED_USER": None,
+    "UNAUTHENTICATED_TOKEN": None,
 }
 
 DATABASES = {
