9b3da2891d5fb9ada6ed11163a6e21adb7ac8c46
김유찬 <a01030163474@gmail.com>
2026-07-28 13:27:47 +0900

    chore: 앱 버전 0.1.7 갱신

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -20,7 +20,7 @@ import 'party_schedule_store.dart';
 import 'scheduler_cache.dart';
 import 'sunday_event_cache.dart';
 
-const appCurrentVersion = '0.1.6';
+const appCurrentVersion = '0.1.7';
 const _mainWindowSize = Size(1280, 860);
 const _mapleProcessNames = {
   'maplestory.exe',
