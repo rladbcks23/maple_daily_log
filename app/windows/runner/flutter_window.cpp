87295df6707e65cf7ddb63c5d6064f9f6c3c8ec2
김유찬 <a01030163474@gmail.com>
2026-07-29 14:48:56 +0900

    fix: 닫기 버튼 백그라운드 숨김 처리 강화

--- app/windows/runner/flutter_window.cpp
+++ app/windows/runner/flutter_window.cpp
@@ -58,6 +58,11 @@ LRESULT
 FlutterWindow::MessageHandler(HWND hwnd, UINT const message,
                               WPARAM const wparam,
                               LPARAM const lparam) noexcept {
+  if (message == WM_CLOSE) {
+    ShowWindow(hwnd, SW_HIDE);
+    return 0;
+  }
+
   // Give Flutter, including plugins, an opportunity to handle window messages.
   if (flutter_controller_) {
     std::optional<LRESULT> result =
